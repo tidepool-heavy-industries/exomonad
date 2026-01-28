d8648372caa8d7cdad5e6af0445cecdee4e4ac43
Inanna Malick <inanna@recursion.wtf>
2026-01-28 14:48:14 -0800

    Migrate DockerCtl to use runSubprocess

--- haskell/control-server/src/ExoMonad/Control/Effects/DockerCtl.hs
+++ haskell/control-server/src/ExoMonad/Control/Effects/DockerCtl.hs
@@ -11,29 +11,27 @@ module ExoMonad.Control.Effects.DockerCtl
   ) where
 
 import Control.Monad.Freer (Eff, interpret, LastMember, sendM)
-import Data.Aeson (eitherDecode)
-import qualified Data.ByteString.Lazy as BL
 import Data.Text (Text)
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import System.Process (readProcessWithExitCode)
-import System.Exit (ExitCode(..))
 
 import ExoMonad.Effects.DockerSpawner
+import ExoMonad.Control.Logging (Logger)
+import ExoMonad.Control.Subprocess (runSubprocessJSON, runSubprocess, SubprocessResult(..), SubprocessError(..))
 
 runDockerCtl
   :: LastMember IO es
-  => FilePath -- ^ Path to docker-ctl binary
+  => Logger
+  -> FilePath -- ^ Path to docker-ctl binary
   -> Eff (DockerSpawner ': es) a
   -> Eff es a
-runDockerCtl binPath = interpret $ \case
-  SpawnContainer cfg -> sendM $ doSpawn binPath cfg
-  StopContainer cid -> sendM $ doStop binPath cid
-  GetContainerStatus cid -> sendM $ doStatus binPath cid
-  ExecContainer cid cmd mWorkdir mUser -> sendM $ doExec binPath cid cmd mWorkdir mUser
+runDockerCtl logger binPath = interpret $ \case
+  SpawnContainer cfg -> sendM $ doSpawn logger binPath cfg
+  StopContainer cid -> sendM $ doStop logger binPath cid
+  GetContainerStatus cid -> sendM $ doStatus logger binPath cid
+  ExecContainer cid cmd mWorkdir mUser -> sendM $ doExec logger binPath cid cmd mWorkdir mUser
 
-doSpawn :: FilePath -> SpawnConfig -> IO (Either DockerError ContainerId)
-doSpawn binPath cfg = do
+doSpawn :: Logger -> FilePath -> SpawnConfig -> IO (Either DockerError ContainerId)
+doSpawn logger binPath cfg = do
   let envArgs = concatMap (\(k, v) -> ["-e", T.unpack k <> "=" <> T.unpack v]) (cfg.scEnv)
       args = [ "spawn"
              , "--issue-id", T.unpack (cfg.scIssueId)
@@ -44,41 +42,31 @@ doSpawn binPath cfg = do
                ++ envArgs
                ++ maybe [] (\c -> "--" : map T.unpack c) (cfg.scCmd)
 
-  (code, stdout, stderr) <- readProcessWithExitCode binPath args ""
-  case code of
-    ExitSuccess -> case eitherDecode (BL.fromStrict $ TE.encodeUtf8 $ T.pack stdout) of
-      Right (res :: ContainerId) -> pure $ Right res
-      Left err -> pure $ Left $ DockerApiError 200 (T.pack err)
-    ExitFailure _ -> pure $ Left $ DockerConnectionError (T.pack stderr)
+  runSubprocessJSON logger "[DockerCtl]" binPath args >>= \case
+    Right res -> pure $ Right res
+    Left err -> pure $ Left $ DockerApiError 200 err.stderr
 
-doStop :: FilePath -> ContainerId -> IO (Either DockerError ())
-doStop binPath (ContainerId cid) = do
+doStop :: Logger -> FilePath -> ContainerId -> IO (Either DockerError ())
+doStop logger binPath (ContainerId cid) = do
   let args = ["stop", T.unpack cid]
-  (code, _stdout, stderr) <- readProcessWithExitCode binPath args ""
-  case code of
-    ExitSuccess -> pure $ Right ()
-    ExitFailure _ -> pure $ Left $ DockerConnectionError (T.pack stderr)
+  runSubprocess logger "[DockerCtl]" binPath args >>= \case
+    SubprocessSuccess _ _ -> pure $ Right ()
+    SubprocessFailure err -> pure $ Left $ DockerConnectionError err.stderr
 
-doStatus :: FilePath -> ContainerId -> IO (Either DockerError ContainerStatus)
-doStatus binPath (ContainerId cid) = do
+doStatus :: Logger -> FilePath -> ContainerId -> IO (Either DockerError ContainerStatus)
+doStatus logger binPath (ContainerId cid) = do
   let args = ["status", T.unpack cid]
-  (code, stdout, stderr) <- readProcessWithExitCode binPath args ""
-  case code of
-    ExitSuccess -> case eitherDecode (BL.fromStrict $ TE.encodeUtf8 $ T.pack stdout) of
-      Right (res :: ContainerStatus) -> pure $ Right res
-      Left err -> pure $ Left $ DockerApiError 200 (T.pack err)
-    ExitFailure _ -> pure $ Left $ DockerConnectionError (T.pack stderr)
+  runSubprocessJSON logger "[DockerCtl]" binPath args >>= \case
+    Right res -> pure $ Right res
+    Left err -> pure $ Left $ DockerApiError 200 err.stderr
 
-doExec :: FilePath -> ContainerId -> [Text] -> Maybe FilePath -> Maybe Text -> IO (Either DockerError ExecResult)
-doExec binPath (ContainerId cid) cmd mWorkdir mUser = do
+doExec :: Logger -> FilePath -> ContainerId -> [Text] -> Maybe FilePath -> Maybe Text -> IO (Either DockerError ExecResult)
+doExec logger binPath (ContainerId cid) cmd mWorkdir mUser = do
   let args = ["exec", T.unpack cid]
            ++ maybe [] (\w -> ["--workdir", w]) mWorkdir
            ++ maybe [] (\u -> ["--user", T.unpack u]) mUser
            ++ ["--"] ++ map T.unpack cmd
   
-  (code, stdout, stderr) <- readProcessWithExitCode binPath args ""
-  case code of
-    ExitSuccess -> case eitherDecode (BL.fromStrict $ TE.encodeUtf8 $ T.pack stdout) of
-      Right (res :: ExecResult) -> pure $ Right res
-      Left err -> pure $ Left $ DockerApiError 200 (T.pack err)
-    ExitFailure _ -> pure $ Left $ DockerExecError (T.pack stderr)
+  runSubprocessJSON logger "[DockerCtl]" binPath args >>= \case
+    Right res -> pure $ Right res
+    Left err -> pure $ Left $ DockerExecError err.stderr
