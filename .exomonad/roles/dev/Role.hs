ab32b763f9eb3b2f6685591b3b4b530a908e7d90
Inanna Malick <inanna@recursion.wtf>
2026-02-03 16:50:53 -0800

    feat: Create .exomonad directory structure and role stubs (#513)
    * feat: Create .exomonad directory structure and role stubs
    
    - Create .exomonad/flake.nix with stub mkWasmRole calls
    - Update .exomonad/config.toml with default and agent roles
    - Update .exomonad/.gitignore
    - Implement nested tool records in roles/dev/Role.hs and roles/tl/Role.hs
    - Create roles/tl/Hooks.hs with example hooks
    - Verify lib/Common.hs exists
    
    Resolves #507
    
    * fix: Correct RoleConfig types and Hooks.hs imports/logic
    
    - Update RoleConfig type signatures to use concrete 'Tools AsHandler'.
    - Fix Hooks.hs to use 'Sem HookEffects' and correct return types.
    - Stub out git dirty checks in Hooks.hs as they are not currently supported by the effect system.
    - Add necessary imports for StopHookOutput and response helpers.

--- .exomonad/roles/dev/Role.hs
+++ .exomonad/roles/dev/Role.hs
@@ -5,22 +5,20 @@ module Role (config) where
 
 import ExoMonad
 
+-- User defines their tool record by COMPOSING framework records
 data Tools mode = Tools
-  { git :: GitTools mode,
-    github :: GitHubTools mode,
-    fs :: FileTools mode
-  }
-  deriving (Generic)
+  { git :: GitTools mode        -- Nested record, NOT individual tools!
+  , github :: GitHubTools mode  -- Nested record
+  , fs :: FileTools mode        -- Nested record
+  } deriving Generic
 
 config :: RoleConfig (Tools AsHandler)
-config =
-  RoleConfig
-    { roleName = "dev",
-      tools =
-        Tools
-          { git = gitTools,
-            github = githubTools,
-            fs = fileTools
-          },
-      hooks = defaultHooks
-    }
+config = RoleConfig
+  { roleName = "dev"
+  , tools = Tools
+      { git = gitTools          -- Use pre-built framework value
+      , github = githubTools    -- Use pre-built framework value
+      , fs = fileTools          -- Use pre-built framework value
+      }
+  , hooks = defaultHooks
+  }
--- .exomonad/roles/tl/Role.hs
+++ .exomonad/roles/tl/Role.hs
@@ -4,25 +4,24 @@
 module Role (config) where
 
 import ExoMonad
+import qualified Hooks
 
+-- TL has more tool groups than dev
 data Tools mode = Tools
-  { git :: GitTools mode,
-    github :: GitHubTools mode,
-    fs :: FileTools mode,
-    agents :: AgentTools mode
-  }
-  deriving (Generic)
+  { git :: GitTools mode
+  , github :: GitHubTools mode
+  , fs :: FileTools mode
+  , agents :: AgentTools mode   -- TL adds agent orchestration
+  } deriving Generic
 
 config :: RoleConfig (Tools AsHandler)
-config =
-  RoleConfig
-    { roleName = "tl",
-      tools =
-        Tools
-          { git = gitTools,
-            github = githubTools,
-            fs = fileTools,
-            agents = agentTools
-          },
-      hooks = defaultHooks
-    }
+config = RoleConfig
+  { roleName = "tl"
+  , tools = Tools
+      { git = gitTools
+      , github = githubTools
+      , fs = fileTools
+      , agents = agentTools
+      }
+  , hooks = Hooks.tlHooks
+  }
