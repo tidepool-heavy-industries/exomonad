13093de9a4ec545a7a2f032415ceb6a69c7b4312
Inanna Malick <inanna@recursion.wtf>
2026-04-10 23:17:36 -0700

    fix: remove trailing whitespace

--- haskell/wasm-guest/src/ExoMonad/Guest/Tools/MergePRTests.hs
+++ haskell/wasm-guest/src/ExoMonad/Guest/Tools/MergePRTests.hs
@@ -4,7 +4,7 @@ module ExoMonad.Guest.Tools.MergePRTests where
 import ExoMonad.Guest.Tool.Class (MCPCallOutput (..))
 import ExoMonad.Guest.Tools.MergePR
 
--- Note: These are manual verification tests since a full test runner 
+-- Note: These are manual verification tests since a full test runner
 -- isn't yet configured for the WASM guest in this environment.
 
 test_render_all_success :: MCPCallOutput
